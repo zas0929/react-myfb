7743ecb7923b3caf424fb347c864dad5c4b92814
zas0929 <rolisovskiy@mail.ru>
2018-10-07 10:00:43 +0300

    Codestyle

--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -7,26 +7,27 @@ import { Consumer } from 'components/HOC/withProfile';
 import Styles from './styles.m.css';
 
 export default class Post extends Component {
-    static propTypes = {
-      comment: PropTypes.string.isRequired,
-      created: PropTypes.number.isRequired
-    }
-    render () {
-        const { comment, created } = this.props;
+  static propTypes = {
+    comment: PropTypes.string.isRequired,
+    created: PropTypes.number.isRequired
+  }
 
-        return (
-          <Consumer>
-            {(context) => (
-              <section className={ Styles.post }>
-                <img src={context.avatar} />
-                <a>{`${context.currentUserFirstName} ${context.currentUserLastName}`}</a>
-                <time>
-                  {moment.unix(created).format('MMMM D h:mm:ss a')}
-                </time>
-                <p>{comment}!</p>
-              </section>
-            )}
-          </Consumer>
-        );
-    }
+  render () {
+    const { comment, created } = this.props;
+
+    return (
+      <Consumer>
+        {(context) => (
+          <section className={ Styles.post }>
+            <img src={context.avatar} />
+            <a>{`${context.currentUserFirstName} ${context.currentUserLastName}`}</a>
+            <time>
+              {moment.unix(created).format('MMMM D h:mm:ss a')}
+            </time>
+            <p>{comment}!</p>
+          </section>
+        )}
+      </Consumer>
+    );
+  }
 }
