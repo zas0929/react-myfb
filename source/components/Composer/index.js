ec1e69c7b09e1f06d01ae72b02bf1c9bff2cb927
zas0929 <rolisovskiy@mail.ru>
2018-10-07 09:56:40 +0300

    Add comments in ui

--- source/components/Composer/index.js
+++ source/components/Composer/index.js
@@ -1,19 +1,69 @@
 // Core
 import React, { Component } from 'react';
+import PropTypes from "prop-types";
 
 import { Consumer } from 'components/HOC/withProfile';
 
 import Styles from './styles.m.css';
 
 export default class Composer extends Component {
+  static propTypes = {
+    _createPost: PropTypes.func.isRequired,
+  }
+
+  state = {
+    comment: ""
+  }
+
+  _updateComment = (event) => {
+    this.setState({
+      comment: event.target.value
+    });
+  }
+
+  _handleFormSubmit = (event) => {
+    event.preventDefault();
+    this._submitComment();
+  }
+
+  _submitComment = () => {
+    const { comment } = this.state;
+
+    if (!comment) {
+      return null;
+    }
+
+    this.props._createPost(comment);
+
+    this.setState({
+      comment: ""
+    });
+  }
+
+  _submitOnEnter = (event) => {
+    const enterKey = event.key === "Enter";
+
+    if (enterKey) {
+      event.preventDefault();
+      this._submitComment();
+    }
+  }
+
   render () {
+    const { comment } = this.state;
+
     return (
       <Consumer>
         {(context) => (
           <section className={ Styles.composer }>
-            <img src={context.avatar}/>
-            <form>
-              <textarea placeholder={`What's on your mind ${context.currentUserFirstName}`} />
+            <img src={context.avatar} />
+            <form onSubmit={this._handleFormSubmit}>
+              <textarea
+                placeholder={`What's on your mind ${context.currentUserFirstName}`}
+                value={comment}
+                onChange={this._updateComment}
+                onKeyPress={this._submitOnEnter}
+              />
               <input type='submit' value='Post' />
             </form>
           </section>
--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,35 +1,50 @@
 // Core
 import React, { Component } from 'react';
+import moment from 'moment';
+
 import StatusBar from 'components/StatusBar';
 import Composer from 'components/Composer';
 import Post from 'components/Post';
 import Spinner from 'components/Spinner';
 
 import Styles from './styles.m.css';
-import avatar from 'theme/assets/lisa';
+import { getUniqueID } from "instruments";
 
 export default class Feed extends Component {
-    state = {
-      posts: [
-          { id: '123', comment: 'Hi there', created: 1526825076849},
-          { id: '321', comment: 'Hello', created: 1526825076855}
-        ],
-      isSpinning: false
-    }
-    render () {
-        const { posts, isSpinning } = this.state;
+  state = {
+    posts: [
+      { id: '123', comment: 'Hi there', created: 1526825076849 },
+      { id: '321', comment: 'Hello', created: 1526825076855 }
+    ],
+    isSpinning: false,
+  }
 
-        const postsJSX = posts.map((post) => {
-            return <Post key={ post.id } { ...post } />
-        });
+  _createPost = (comment) => {
+    const post = {
+      id:      getUniqueID(),
+      created: moment.now(),
+      comment,
+    };
 
-        return (
-            <section className={ Styles.feed }>
-              <Spinner isSpinning={isSpinning} />
-              <StatusBar />
-              <Composer />
-              {postsJSX}
-          </section>
-        );
-    }
+    this.setState(({ posts }) => ({
+      posts: [post, ...posts],
+    }));
+  }
+
+  render () {
+    const { posts, isSpinning } = this.state;
+
+    const postsJSX = posts.map((post) => {
+      return <Post key={ post.id } { ...post } />;
+    });
+
+    return (
+      <section className={Styles.feed }>
+        <Spinner isSpinning={isSpinning} />
+        <StatusBar />
+        <Composer _createPost={this._createPost} />
+        {postsJSX}
+      </section>
+    );
+  }
 }
